394ae066b2a212f1393282262f3bb7b5be08ea25
Tianhai Liu <liu@aicas.de>
2026-02-09 15:43:49 +0100

    Update copyright headers to 2026 across all source files

--- src/test/java/com/aicas/tristan/cloudconnector/MqttClientWrapperTest.java
+++ src/test/java/com/aicas/tristan/cloudconnector/MqttClientWrapperTest.java
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------*
- * Copyright 2024, aicas GmbH; all rights reserved.
+ * Copyright 2026, aicas GmbH; all rights reserved.
  * This header, including copyright notice, may not be altered or removed.
  *------------------------------------------------------------------------*/
 package com.aicas.tristan.cloudconnector;
